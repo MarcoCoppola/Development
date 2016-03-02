839614e466c0d1ff8a342d3458e1c6317bd9232a
MaxTenco <ingcoppolawork@gmail.com>
2016-03-02 12:35:31 +0100

    Create endpoint.js

---
+++ AngulaJS/resources/endpoint.js
@@ -0,0 +1,71 @@
+(function()
+{
+    'use strict';
+
+    angular
+        .module('')
+        .factory('endpointService', endpointService);
+
+    endpointService.$inject = [];
+
+    /* @ngInject */
+    function endpointService()
+    {
+        return {
+            service: service
+        };
+
+
+        //----------------------------------------------------------
+        // PUBLIC METHODS
+        //----------------------------------------------------------
+        function service()
+        {
+            var resource = '/service',
+               param = {
+                        foo: foo,
+                        bar: bar
+                    };
+
+            return getEndpoint(resource, params);
+        }
+
+
+        //----------------------------------------------------------
+        // PRIVATE METHODS
+        //----------------------------------------------------------
+        /**
+         * Generate the endpoint. 
+         *
+         * @param resource
+         * @param {array} [params = {}]
+         * @returns {string}
+         */
+        function getEndpoint(resource,params)
+        {
+            var endpoints = '';
+            params = params || {};
+
+            // Add resource
+            endpoints += resource;
+            endpoints += '?';
+
+            // Add parameters
+            if (Object.keys(params).length)
+            {
+                Object.keys(params).forEach(function(key)
+                {
+                    var value = params[key];
+
+                    if (value != null)
+                    {
+                        endpoints += key + '=' + value + '&';
+                    }
+                });
+            }
+
+            return endpoints;
+        }
+    }
+})();
+
