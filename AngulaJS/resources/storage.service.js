ae206b20ed479ba854ff8c71549a72d315872383
MaxTenco <ingcoppolawork@gmail.com>
2016-03-11 11:10:18 +0100

    Update storage.service.js

--- AngulaJS/resources/storage.service.js
+++ AngulaJS/resources/storage.service.js
@@ -3,7 +3,7 @@
     'use strict';
 
     angular
-        .module('calendario-corsi')
+        .module('')
         .factory('storageService', storageService);
 
     storageService.$inject = ['$log', '$window'];
@@ -16,10 +16,9 @@
 
         return {
             set: set,
-            //get: getFromLocalStorage,
-            //keys: getKeysForLocalStorage,
-            //remove: removeFromLocalStorage,
-            //clearAll: clearAllFromLocalStorage,
+            get: get,
+            remove: remove,
+            clearAll: clearAll
         };
 
 
@@ -56,6 +55,71 @@
             }
 
             return true;
+        }
+
+
+        /**
+         * Retrives a value from local storage.
+         *
+         * @param key
+         * @returns {null}
+         */
+        function get(key)
+        {
+
+            var item = $window.localStorage.getItem(key);
+
+            if (!item || item === 'null')
+            {
+                return null;
+            }
+
+            try
+            {
+                return JSON.parse(item);
+            } catch (e)
+            {
+                $log.warn('Can not get:', key, '\n', e);
+                return null;
+            }
+        }
+
+
+        /**
+         * Removes an item from local storage.
+         *
+         * @param key
+         * @returns {boolean}
+         */
+        function remove(key)
+        {
+            try
+            {
+                $window.localStorage.removeItem(key);
+            }
+            catch (e)
+            {
+                $log.error('Can not remove:', key, '\n', e);
+                return false
+            }
+
+            return true;
+        }
+
+
+        /**
+         * Removes all data from local storage.
+         */
+        function clearAll()
+        {
+            try
+            {
+                $window.localStorage.clear();
+            }
+            catch (e)
+            {
+                $log.error('Can not clear local storage:', '\n', e);
+            }
         }
 
         //----------------------------------------------------------
