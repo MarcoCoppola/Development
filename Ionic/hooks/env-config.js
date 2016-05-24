0e000067664b7e155d22ca402880602a35133208
MaxTenco <ingcoppolawork@gmail.com>
2016-05-24 12:41:34 +0200

    Create env-config.js

---
+++ Ionic/hooks/env-config.js
@@ -0,0 +1,24 @@
+{
+  "env": "stage",
+
+  "dev":{
+    "sensitiveInfoEnabled": true,
+    "logEnabled": false,
+    "debugEnabled": true,
+    "apiUrl": "'https://intraweb-svil.enel.com/atlante'"
+  },
+
+  "stage": {
+    "sensitiveInfoEnabled": true,
+    "logEnabled": true,
+    "debugEnabled": true,
+    "apiUrl": "'https://intraweb-coll.enel.com/atlante'"
+  },
+
+  "prod": {
+    "sensitiveInfoEnabled": false,
+    "logEnabled": false,
+    "debugEnabled": false,
+    "apiUrl": "'https://intraweb.enel.com/atlante'"
+  }
+}
